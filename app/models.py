e4783011eb5042d98b04811756390fafd995fbee
Nishan Das <21084024+42eggs@users.noreply.github.com>
2023-02-13 18:06:28 +0530

    models updated

--- app/models.py
+++ app/models.py
@@ -3,48 +3,79 @@ from .database import Base
 from sqlalchemy.orm import relationship
 
 
-class Post(Base):
-    __tablename__ = "posts"
+class Dogs(Base):
+    __tablename__ = "dogs"
 
+    #auto generated
     id = Column(Integer, primary_key=True, index=True, nullable=False)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    published = Column(Boolean, server_default="TRUE", nullable=False)
-    created_at = Column(
-        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
-    )
-    user_id = Column(
+
+    #user input : mandatory
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    age_months = Column(Integer, nullable=False)
+
+    #user input : optional
+    disabled = Column(Boolean, server_default='FALSE', nullable=False)
+    street_rescue = Column(Boolean, server_default='FALSE', nullable=False)
+    potty_trained = Column(Boolean, server_default='FALSE', nullable=False)
+    
+    # Groups (months): 0-2: 1, 2-6: 2, 6-12: 3, 12-24: 4, 24-48: 5, 48-96: 6, 96+: 7
+    # TODO: find a better way to do this
+    age_group = Column(Integer, nullable=False)
+    
+    #to be taken automatically when user is authenticated
+    owner_id = Column(
         Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
 
-    user = relationship("User")
+    #may be useful later
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
+
+    user = relationship("Users")
 
 
-class User(Base):
+class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False)
     password = Column(String, nullable=False)
+    phone= Column(String, nullable=False)
     created_at = Column(
         TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
 
 
-class Vote(Base):
-    __tablename__ = "votes"
+class AdoptionRequests(Base):
+    __tablename__ = "adoption_requests"
 
-    user_id = Column(
+    #taken from dogs's owner_id
+    requested_to = Column(
         Integer,
         ForeignKey("users.id", ondelete="CASCADE"),
         nullable=False,
         primary_key=True,
     )
-    post_id = Column(
+    #taken from dogs's id
+    dog_requested = Column(
         Integer,
-        ForeignKey("posts.id", ondelete="CASCADE"),
+        ForeignKey("dogs.id", ondelete="CASCADE"),
         nullable=False,
         primary_key=True,
+    )
+    #taken from authenticated user's id or redirected to login
+    requested_from = Column(
+        Integer,
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        primary_key=True,
+    )
+
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
 
     # user = relationship("User")
