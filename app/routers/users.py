5e0f6a21620013871d63dc29e9d57a51ae820ec2
Nishan Das <21084024+42eggs@users.noreply.github.com>
2023-02-16 04:10:15 +0530

    added auth to /users get route

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,6 @@
 from fastapi import status, HTTPException, Depends, APIRouter
+
+from app import oauth2
 from .. import models, schemas, utils
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -24,14 +26,15 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         )
 
 
-@router.get("/{id}", response_model=schemas.UserResponse)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
+@router.get("/", response_model=schemas.UserResponse)
+def get_user(
+    db: Session = Depends(get_db),
+    current_user=Depends(oauth2.get_current_user),
+):
+    # if not user:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_404_NOT_FOUND,
+    #         detail=f"User with id: {id} does not exist",
+    #     )
 
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id: {id} does not exist",
-        )
-
-    return user
+    return db.query(models.User).filter(models.User.id == current_user.id).first()
