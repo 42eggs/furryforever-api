2f1903127487d6913d121273a12fa00a9968f087
Nishan Das <21084024+42eggs@users.noreply.github.com>
2023-02-24 02:46:27 +0530

    it works but how??

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 API development in progress: [Furry Forever API](http://64.227.129.68/docs)
+
+
+Is the workflow working? 
+much wow
+I don't remember pytest creating its own tables, wow
--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(adoption_requests.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to Furry Forever! sadsadad"}
+    return {"message": "Welcome to Furry Forever!"}
